36769e0f7e4dbc6deafea6578f718dbe01f17ab6
rushikesh2k04 <rushikesh9.2004@gmail.com>
2025-05-04 05:42:23 +0530

    locanet to testnet

--- projects/nftcreate/src/main.tsx
+++ projects/nftcreate/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles/App.css'
 import ErrorBoundary from './components/ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
